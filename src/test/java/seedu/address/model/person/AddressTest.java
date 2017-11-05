b82f38a30d340f30d06f0efc5d7dc21b488b67a3
Alex Foo <alexfoodw@gmail.com>
2017-11-05 18:36:05 +0800

    edit test cases to accept null values

--- src/test/java/seedu/address/model/person/AddressTest.java
+++ src/test/java/seedu/address/model/person/AddressTest.java
@@ -9,10 +9,6 @@ public class AddressTest {
 
     @Test
     public void isValidAddress() {
-        // invalid addresses
-        assertFalse(Address.isValidAddress("")); // empty string
-        assertFalse(Address.isValidAddress(" ")); // spaces only
-
         // valid addresses
         assertTrue(Address.isValidAddress("Blk 456, Den Road, #01-355"));
         assertTrue(Address.isValidAddress("-")); // one character
